c49e3b183bf1af838dad749e6fef03d4467d5837
Sadiul Hakim <sadiulhakim@gmail.com>
2024-08-11 21:48:05 +0600

    Start

---
+++ src/main/java/xyz/sadiulhakim/oauthapigateway/OauthApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package xyz.sadiulhakim.oauthapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/sadiulhakim/oauthapigateway/OauthApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.sadiulhakim.oauthapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
